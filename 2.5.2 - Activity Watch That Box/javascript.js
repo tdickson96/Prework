c23824da6bcd2aacb5b1573370e30fb6a09079a0
LocalGraduate <tdickson91@gmail.com>
2022-03-16 14:35:59 -0400

    update on activities and basics

---
+++ 2.5.2 - Activity Watch That Box/javascript.js
@@ -0,0 +1,28 @@
+document.getElementById("shrinkButton").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "25px";
+    document.getElementById("box").style.width = "25px";
+
+});
+
+document.getElementById("growButton").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+    document.getElementById("box").style.width = "250px";
+
+});
+
+document.getElementById("blueButton").addEventListener("click", function)(){
+    
+    document.getElementById("box").style.color = "blue";
+
+});
+
+document.getElementById("fadeButton").addEventListener("click", function)(){
+    document.getElementById("box").
+});
+document.getElementById("resetButton").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+
+});
